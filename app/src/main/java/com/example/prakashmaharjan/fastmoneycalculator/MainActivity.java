31257d6906ce709bd905af84a979734d846cf73b
prakashmaharjan <prakashmaharjan20@gmail.com>
2019-08-25 17:18:55 +0545

    gradle update

--- app/src/main/java/com/example/prakashmaharjan/fastmoneycalculator/MainActivity.java
+++ app/src/main/java/com/example/prakashmaharjan/fastmoneycalculator/MainActivity.java
@@ -1,15 +1,13 @@
 package com.example.prakashmaharjan.fastmoneycalculator;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
